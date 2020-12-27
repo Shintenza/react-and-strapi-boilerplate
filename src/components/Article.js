b1f35b4cdb7b418127e34c0f01047427aedac898
Shintenza <kam.kuziora@gmail.com>
2020-12-27 22:50:08 +0100

    simple styling

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,8 +2,8 @@ const Article = (props) => {
   const dateInfo = `${props.date.getHours()}.${props.date.getMinutes()} ${props.date.getDate()}.${props.date.getMonth()}.${props.date.getFullYear()}`;
   return (
     <div className="article">
-      <div className="articleDate">{dateInfo}</div>
       <div className="articleTitle">{props.title}</div>
+      <div className="articleDate">Published: {dateInfo}</div>
       <div className="articleDesc">{props.description}</div>
       <button className="readMore" onClick={(e) => props.previewMode(props, e)}>
         Read more
--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,10 +1,10 @@
 const Preview = (props) => {
   const dateInfo = `${props.date.getHours()}.${props.date.getMinutes()} ${props.date.getDate()}.${props.date.getMonth()}.${props.date.getFullYear()}`;
   return (
-    <div className="article">
-      <div className="articleDate">{dateInfo}</div>
-      <div className="articleTitle">{props.title}</div>
-      <div className="articleDesc">{props.description}</div>
+    <div className="preview">
+      <div className="previewTitle">{props.title}</div>
+      <div className="previewDate">Published: {dateInfo}</div>
+      <div className="previewDesc">{props.description}</div>
       <button className="readMore" onClick={(e) => props.previewMode(null, e)}>
         Go back
       </button>
